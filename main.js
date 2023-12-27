f0506da87eab655740542bc610bfd4250acf4da6
Emerson Teixeira <emerson@exemplo.com>
2023-12-27 12:37:20 -0300

    first commit

--- main.js
+++ main.js
@@ -1,67 +1,36 @@
-const form = document.getElementById('form-atividade');
-const imgAprovado = ('<img src="aprovado.png" alt="emoji festejando"/>')
-const imgReprovado = ('<img src="reprovado.png" alt="emoji triste"/>')
-const atividades = [];
-const notas = [];
-const spanAprovado = '<span class="resultado aprovado">aprovado</span>'
-const spanReprovado = '<span class="resultado reprovado">Reprovado</span>'
-const notaMinima = parseFloat(prompt('Digite a nota mínima:'))
+function adicionarContato() {
+    const nome = document.getElementById('nome').value;
+    const telefone = document.getElementById('telefone').value;
 
+    if (nome && telefone) {
+        const tabelaContatos = document.getElementById('tabelaContatos');
+        const tbody = tabelaContatos.querySelector('tbody');
 
-let linhas = '';
+        const linhasExistentes = tbody.getElementsByTagName('tr');
+        let telefoneRepetido = false;
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+        for (const linha of linhasExistentes) {
+            const colunaTelefone = linha.getElementsByTagName('td')[1].textContent;
 
-    adicionarLinha()
-    atualizarLinha()
-    atualizarMediaFinal()
-});
+            if (colunaTelefone === telefone) {
+                telefoneRepetido = true;
+                break;
+            }
+        }
 
-function adicionarLinha() {
-    const inputNomeAtividade = document.getElementById('nome-atividade');
-    const inputNotaAtividade = document.getElementById('nota-atividade');
+        if (telefoneRepetido) {
+            alert('Telefone já existe na agenda.');
+        } else {
+            const novaLinha = document.createElement('tr');
+            novaLinha.innerHTML = `<td>${nome}</td><td>${telefone}</td>`;
+            tbody.appendChild(novaLinha);
 
-    if (atividades.includes(inputNomeAtividade.value)) {
-        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
+            document.getElementById('nome').value = '';
+            document.getElementById('telefone').value = '';
+        }
     } else {
-
-    atividades.push(inputNomeAtividade.value);
-    notas.push(parseFloat(inputNotaAtividade.value));
-
-    let linha = '<tr>'
-    linha += `<td> ${inputNomeAtividade.value} </td>`;
-    linha += `<td> ${inputNotaAtividade.value} </td>`;
-    linha += `<td> ${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado} </td>`;
-    linha += `</td>`;
-
-    linhas += linha 
-}
-
-    inputNomeAtividade.value = '';
-    inputNotaAtividade.value = '';
-}
-
-function atualizarLinha() {
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-}
-
-function atualizarMediaFinal() {
-    const mediaFinal = calculaMediaFinal()
-
-    document.getElementById('Media-Final-Valor').innerHTML = mediaFinal.toFixed(2)
-    document.getElementById('Media-Final-Resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado ;
-}
-
-function calculaMediaFinal() {
-    
-    let somasDasNotas = 0
-
-    for (let i = 0; i < notas.length; i++) {
-        somasDasNotas += notas[i]
+        alert('Por favor, preencha todos os campos.');
     }
-
-    return somasDasNotas / notas.length
-
 }
+
+
