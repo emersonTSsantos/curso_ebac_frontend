19308e7c55e78884187e4073bd52b7c20301b416
Emerson Teixeira <emerson@exemplo.com>
2024-02-12 17:09:34 -0300

    realizando tarefas com gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,62 +1,35 @@
-//executando automações publicas e privadas 
-//executando funções em séries usando o camando gulp.series e aplicando as funções que estarão dentro da automação
-/*
-
-const gulp = require('gulp');
-
-function funcaoPadrao(callback) {
-    console.log('executando via gulp');
-    callback();
-}
-
-function dizOi(callback) {
-    console.log('olá,gulp');
-    dizTchau();
-    callback();
-}
-
-function dizTchau() {
-    console.log('tchau gulp');
-}
-
-exports.default = gulp.series(funcaoPadrao, dizOi);
-exports.dizOi = dizOi
-
-
-*/
-
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require ('sass'));
+const imagemin = require('gulp-imagemin')
+const uglify = require('gulp-uglify');
 
 function compilaSass() {
-    return gulp.src('./source/styles/*.scss')
-        .pipe(sass().on('error', sass.logError))
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sass({
+            outputStyle:'compressed'
+        }).on('error', sass.logError))
         .pipe(gulp.dest('./build/styles'));
 }
 
-function funcaoPadrao(callback) {
-    setTimeout (function() {
-
-        console.log('executando via gulp');
-        callback();
-
-    }, 2000)
-    
+function comprimeImagens() {
+    return gulp.src('./source/imagens/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/imagens'))
 }
 
-function dizOi(callback) {
-    console.log('olá,gulp');
-    dizTchau();
-    callback();
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
 }
 
-function dizTchau() {
-    console.log('tchau gulp');
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false} , gulp.series(compilaSass) );
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false} , gulp.series(comprimeJavaScript) );
+    gulp.watch('./source/imagens/*', {ignoreInitial: false} , gulp.series(comprimeImagens) );
 }
 
-exports.default = gulp.parallel(funcaoPadrao, dizOi);
-exports.dizOi = dizOi
-exports.sass = compilaSass;
+
 
 
 
